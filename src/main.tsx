c68f40916baf773f8662f861a8ddfeb9b92db49c
Zackary Contreras <zackaryc007@gmail.com>
2023-09-22 22:31:43 -0700

    hardcode clerk key

--- src/main.tsx
+++ src/main.tsx
@@ -15,10 +15,17 @@ import {
 } from "@clerk/clerk-react";
 import { AnimatePresence } from "framer-motion";
 
-if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+/**
+ * Setting the clerk publishable key in .env is ideal but having trouble with netlify deployments and since the variables are
+ * statically replaced anyways, I will hardcode the key in here. Clerk explicity states that the publishable key can be safely
+ * used in frontend code and shared without being kep secret.
+ * */
+// if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key");
+// }
+// const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const clerkPubKey = "pk_test_c2Vuc2libGUtZ3J1Yi04OS5jbGVyay5hY2NvdW50cy5kZXYk";
 
 // Create react-query client
 const queryClient = new QueryClient();
